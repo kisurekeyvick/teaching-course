0ab8caadd5a5498f9cc1179f283da8ffbfdd0688
kisurekeyvick <zttaijue1207@163.com>
2019-09-03 11:08:52 +0800

    feat(src/containers/book/latestUpload/*):修改最新上传组件

---
+++ src/containers/book/latestUpload/index.config.ts
@@ -0,0 +1,43 @@
+import noData from 'assets/images/noData.png';
+
+export const imgList = {
+    noData
+}
+
+export const list = [
+    {
+        title: '2019育人精彩瞬间',
+        type: 'doc',
+        contributors: 'ly0915',
+        time: '半个月前',
+        id: '1'
+    },
+    {
+        title: '1.JPG',
+        type: 'pic',
+        contributors: 'MX1@@T1',
+        time: '1个月前',
+        id: '2'
+    },
+    {
+        title: '口耳目.doc',
+        type: 'doc',
+        contributors: 'nice fish',
+        time: '2个月前',
+        id: '3'
+    },
+    {
+        title: '《查字典》教学目目目目目目目目目目目目目目目目目目目目目目目目目目目目目目目',
+        type: 'doc',
+        contributors: 'nice fish',
+        time: '3个月前',
+        id: '4'
+    },
+    {
+        title: '四季教案',
+        type: 'doc',
+        contributors: 'nice fish',
+        time: '4个月前',
+        id: '5'
+    },
+]
--- src/containers/book/latestUpload/index.tsx
+++ src/containers/book/latestUpload/index.tsx
@@ -1,16 +1,64 @@
 import * as React from 'react';
 import './index.scss';
 import { ILatestUploadProps } from '../interface';
+import { list, imgList } from './index.config';
+import { Icon } from 'antd';
 
 export default class LatestUploadContainer extends React.PureComponent<ILatestUploadProps, any> {
     constructor(public props: ILatestUploadProps) {
         super(props);
+
+        this.state = {
+            list: []
+        };
+    }
+
+    componentDidMount() {
+        this.loadLatestUploadList();
+    }
+
+    /** 
+     * @func
+     * @desc 加载最新上传列表
+     */
+    public loadLatestUploadList = () => {
+        this.setState({
+            list: [...list]
+        });
+    }
+
+    /** 
+     * @func
+     * @desc 查看book详情
+     */
+    public showBookDetail = (book: any) => {
+
     }
 
     public render() {
         return <div className='latestUpload-box'>
                     <div className='latestUpload-title'>
                         <p>最新上传</p>
+                    </div>
+                    <div className='latestUpload-list'>
+                        {
+                            this.state.list.length === 0 ?
+                            <div className='noData'>
+                                <img alt='noData' src={imgList.noData}/>
+                            </div> :
+                            this.state.list.map((item: any) => {
+                                return <div className='latestUpload-list-item' key={item.id} onClick={() => this.showBookDetail(item)}>
+                                            <div className='list-item-top'>
+                                                { item.type === 'doc' && <Icon className='list-item-icon' type="file-word" /> }
+                                                { item.type === 'pic' && <Icon className='list-item-icon' type="picture" />}
+                                                <span>{item.title}</span>
+                                            </div>
+                                            <div className='list-item-bottom'>
+                                                <p>{item.contributors} 上传于{item.time}</p>
+                                            </div>
+                                        </div>
+                            })
+                        }
                     </div>
                 </div>
     }
